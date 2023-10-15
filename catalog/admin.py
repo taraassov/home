6f6f0c87966fea1468f7b3de5acf7fe77103d98c
taraassov <taraassov03@gmail.com>
2023-10-15 14:30:02 +0500

    admin

---
+++ catalog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from catalog.models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'purchase_price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
